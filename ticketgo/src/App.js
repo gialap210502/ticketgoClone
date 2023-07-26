1a3170684d15ee995313a6f163a28e8cddcc2c1c
gialap210502 <89369650+gialap210502@users.noreply.github.com>
2023-07-26 15:30:29 +0700

    nav not done

--- ticketgo/src/App.js
+++ ticketgo/src/App.js
@@ -2,17 +2,20 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer/Footer";
+import Nav from "./Components/Nav/Nav";
+
 
 function App() {
   return (
     <Router>
+      <Nav />
       {/* <NavBar />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
       </Routes> */}
-      <Footer />
+      {/* <Footer /> */}
     </Router>
   );
 }
---
+++ ticketgo/src/Components/Nav/Nav.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Nav.css';
+const Nav = () => {
+    return (
+        <div id="header" className="fixed">
+            <div className="container">
+                <div className="row_pc">
+                    <div className="row">
+                        <div className="col-xs-8 col-sm-6 col-md-3 col-lg-4 auto_width_mb">
+                            <div id="logo">
+                                <a href="https://ticketgo.vn">
+                                    <img src="../../../logo3.png" alt="bán vé sự kiện" />
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-xs-8 col-sm-6 col-md-3 col-lg-4 auto_width_mb">
+                            <form class="d-flex">
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="Your Email" name="email" />
+                                    <button class="input-group-text"><i class="fa-solid fa-magnifying-glass" style={{ color: '#9b9da1' }}></i></button>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="col-xs-8 col-sm-6 col-md-4 col-lg-4 header_bar">
+                            <ul class="nav">
+                                <li class="nav-item">
+                                    <a class="nav-link disabled" href="#">Login</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link disabled" href="#"> Register</a>
+                                </li>
+                                <li class="nav-item">
+                                    <div class="nav-link dropdown">
+                                        <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
+                                            
+                                        </button>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Link 1</a></li>
+                                            <li><a class="dropdown-item" href="#">Link 2</a></li>
+                                            <li><a class="dropdown-item" href="#">Link 3</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Nav;
